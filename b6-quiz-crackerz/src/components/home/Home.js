966e0926ae0e9cd0e7bcf8067d0c50ca6df47443
aminulhaque98 <aminulhaque419@gmail.com>
2022-10-12 08:54:00 +0530

    complate home component

--- b6-quiz-crackerz/src/components/home/Home.js
+++ b6-quiz-crackerz/src/components/home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Homes from '../homes/Homes';
+import './Home.css';
 
 const Home = () => {
     const quizTopics = useLoaderData();
@@ -8,7 +9,7 @@ const Home = () => {
     return (
         <div>
             <h3 className='text-3xl text-center'>common header</h3>
-            <div className='flex'>
+            <div className='flex homeComponent '>
                 {
                     quizTopics.data.map(quizTopic => <Homes key={quizTopic.id}
                         quizTopic={quizTopic}
--- b6-quiz-crackerz/src/components/homes/Homes.js
+++ b6-quiz-crackerz/src/components/homes/Homes.js
@@ -4,9 +4,12 @@ const Homes = ({ quizTopic }) => {
     console.log(quizTopic)
     return (
         <div>
-            <h3>{quizTopic.name}</h3>
-            <img src={quizTopic.logo} alt="" />
-            <button>questions</button>
+            <div className=''>
+                <h3 className='absolute text-black-900 hover:text-white text-3xl mx-16 my-16'>{quizTopic.name}</h3>
+                <button className='absolute mx-48 my-56 bg-sky-500/100 hover:bg-rose-900 text-3xl text-white p-2 rounded-lg'>Questions</button>
+            </div>
+            <img className='static' src={quizTopic.logo} alt="" />
+
         </div>
     );
 };
