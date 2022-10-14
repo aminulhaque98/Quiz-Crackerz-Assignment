dde5fb8454241ba8518786eacc48a560d13cd939
aminulhaque98 <aminulhaque419@gmail.com>
2022-10-14 05:54:25 +0530

    set my info

--- b6-quiz-crackerz/src/components/aboutUs/About.js
+++ b6-quiz-crackerz/src/components/aboutUs/About.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h1>this is about componant</h1>
+        <div className='flex text-center justify-center m-12 gap-12'>
+
+            <div className='text-blue-600 text-xl'>
+                <h3 className='text-4xl '>Aminul Haque Nihad</h3>
+                <small>Web Devoloper</small>
+                <p>I Am A Junior Font End Web Devoloper Lives  In Habiganj. <span className='text-2xl text-black font-bold'>My personal information.</span> </p>
+            </div>
+            <div>
+                <img className='w-48 rounded-lg drop-shadow-2xl' src={require('../../Aminulpic120x150.jpg')} alt="" />
+            </div>
+
         </div>
     );
 };
